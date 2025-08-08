6174b7b6d9a22f01e0163d7b6c6219c5415aa670
Muhammad Haris Khan <104778479+ali-haris@users.noreply.github.com>
2025-08-08 10:26:13 +0500

    Time: 3 ms (26.40%) | Memory: 17.8 MB (78.99%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        # Loop through each character index of the first word
+        for i in range(len(strs[0])):
+            # Get the character at index i in the first word
+            char = strs[0][i]
+            
+            # Compare it with the same index in all other words
+            for s in strs[1:]:
+                # If index i is out of range or characters don’t match
+                if i >= len(s) or s[i] != char:
+                    return strs[0][:i]  # Return common prefix up to index i
+
+        # If no mismatch found, entire first word is the prefix
+        return strs[0]
