67c2600f47aaeb28fd59259e3534f8e5dc510059
Muhammad Haris Khan <104778479+ali-haris@users.noreply.github.com>
2025-09-07 11:49:08 +0500

    Time: 17 ms (40.54%), Space: 22.6 MB (25.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -7,3 +7,19 @@ class Solution:
                 key[ord(c) - ord("a")] += 1
             group[tuple(key)].append(w)
         return list(group.values())
+
+
+
+# class Solution:
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         anagram_map = defaultdict(list)
+#         result = []
+
+#         for s in strs:
+#             sorted_s = tuple(sorted(s))
+#             anagram_map[sorted_s].append(s)
+
+#         for value in anagram_map.values():
+#             result.append(value)
+
+#         return result
