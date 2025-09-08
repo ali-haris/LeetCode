74113c668f76b33a1f1dc9af6b48683fd182371f
Muhammad Haris Khan <104778479+ali-haris@users.noreply.github.com>
2025-09-08 23:28:36 +0500

    Time: 0 ms (100%), Space: 23 MB (83.27%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,25 +1,25 @@
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
-        def helper(left: int, right: int) -> None:
-            # Base case
-            if left >= right:
-                return
+    # def reverseString(self, s: List[str]) -> None:
+    #     def helper(left: int, right: int) -> None:
+    #         # Base case
+    #         if left >= right:
+    #             return
             
-            # Swap
-            s[left], s[right] = s[right], s[left]
+    #         # Swap
+    #         s[left], s[right] = s[right], s[left]
             
-            # Recursive step
-            helper(left + 1, right - 1)
+    #         # Recursive step
+    #         helper(left + 1, right - 1)
         
-        # Initial call
-        helper(0, len(s) - 1)
+    #     # Initial call
+    #     helper(0, len(s) - 1)
         
    # normal     
-# def reverseString(s: List[str]) -> None:
-#     left, right = 0, len(s) - 1
-    
-#     # Swap characters from both ends moving towards the middle
-#     while left < right:
-#         s[left], s[right] = s[right], s[left]  # swap
-#         left += 1
-#         right -= 1
+    def reverseString(self,s: List[str]) -> None:
+        left, right = 0, len(s) - 1
+        
+        # Swap characters from both ends moving towards the middle
+        while left < right:
+            s[left], s[right] = s[right], s[left]  # swap
+            left += 1
+            right -= 1
