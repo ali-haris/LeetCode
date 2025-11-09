8146db537a8d835616ea4b1c791b20fef8df817c
Muhammad Haris Khan <104778479+ali-haris@users.noreply.github.com>
2025-11-09 22:37:49 +0500

    Time: 0 ms (100%), Space: 17.8 MB (34.37%) - LeetHub

---
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        a, b = num1, num2
+        ops = 0
+        while a > 0 and b > 0:
+            if a < b:
+                a, b = b, a
+            ops += a // b
+            a %= b
+        return ops
