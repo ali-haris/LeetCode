2456595ca005e19727933a4b15cd33c3ac7d3497
Muhammad Haris Khan <104778479+ali-haris@users.noreply.github.com>
2026-03-30 16:31:49 +0500

    Time: 1754 ms (14.08%), Space: 19.7 MB (89.16%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_map = {}  # Hash table to store number and its index
-        for i, num in enumerate(nums):
-            complement = target - num  # Find the complement
-            if complement in num_map:
-                return [num_map[complement], i]  # Return indices of complement and current number
-            num_map[num] = i  # Store the number with its index
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
