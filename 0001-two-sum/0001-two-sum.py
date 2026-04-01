0d57ac7ff99e846c0a4e29f9bac6e9a7b4b0f8b5
Muhammad Haris Khan <104778479+ali-haris@users.noreply.github.com>
2026-04-01 18:16:21 +0500

    Time: 0 ms (100%), Space: 20.5 MB (51.86%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,16 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        # Dictionary to store number -> index
         ans ={}
 
+        # Loop through the array
         for i in range(len(nums)):
-
+             # Calculate the number we need to reach target
             y = target - nums[i]
 
             if y in ans:
 
                 return [ans[y],i]
 
+            # Otherwise, store current number with its index
             ans[nums[i]] = i
-
 
